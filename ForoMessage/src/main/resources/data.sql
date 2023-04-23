52803923960eeafd1eeb23cba432d7bb566f965d
Adrian-REH <adrianherrera.r.e@gmail.com>
2023-04-23 23:23:54 +0200

    Create New properties by Test with PostgresSQL and Create data.sql file

---
+++ ForoMessage/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into role (description, name) VALUES ('Admin role', 'ADMIN');
+insert into role (description, name) VALUES ('Manager role', 'MANAGER');
+insert into role (description, name) VALUES ('User role', 'USER');
+
+insert into ob_user(username, email, password) VALUES ('adrian', 'adrian@gmail.com', '$2a$10$Uum1/wmFffQ2iz8/MD2gG.Mwrtigik6wyw6H/2NsXKGZbnjfUv96.');
+insert into user_roles(user_id, role_id) VALUES (1, 3);
+
+insert into ob_user(username, email, password) VALUES ('adrian2', 'adrian@gmail.com2', '$2a$10$Uum1/wmFffQ2iz8/MD2gG.Mwrtigik6wyw6H/2NsXKGZbnjfUv96.');
+insert into user_roles(user_id, role_id) VALUES (2, 1);
