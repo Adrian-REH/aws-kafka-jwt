674e202fc55a9542bca843b203415f33f00e21d4
Adrian Herrera <64231248+Adrian-REH@users.noreply.github.com>
2023-04-23 21:38:48 +0200

    Add files via upload

---
+++ ForoMessage/src/test/java/com/example/awskafkajwt/AwsKafkaJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awskafkajwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsKafkaJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
